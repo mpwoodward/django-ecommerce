b6089b65f89b70ec94e664a9cfdacb488ede4c94
Matt Woodward <matt@mattwoodward.com>
2012-08-26 11:26:23 -0700

    initial commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -93,7 +93,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'djangodblog.DBLogMiddleware',
+    'djangodblog.middleware.DBLogMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
